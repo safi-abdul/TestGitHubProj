59b56826fd53c053b883c6284a3f86492096d205
Safi Muktadar Abdul <safi-abdul@users.noreply.github.com>
2020-03-15 22:32:23 -0500

    Add project files.

---
+++ ConsoleGitProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleGitProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
